4500a64ad41bc2eddc08f266c430ffc40f4d8e37
Tom Klaver <tomklav@gmail.com>
2018-06-04 17:28:13 +0200

    adding tslint

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,55 @@
 import './style.scss';
 import * as d3 from 'd3';
 import { Calculator } from './examples/Calculator';
-import { ProvenanceGraph, ProvenanceTracker, ProvenanceGraphTraverser, ReversibleAction, IrreversibleAction, ActionFunctionRegistry, StateNode, RootNode, ProvenanceNode } from '@visualstorytelling/provenance-core';
+import {
+  ProvenanceGraph,
+  ProvenanceTracker,
+  ProvenanceGraphTraverser,
+  ReversibleAction,
+  IrreversibleAction,
+  ActionFunctionRegistry,
+  StateNode,
+  RootNode,
+  ProvenanceNode,
+} from '@visualstorytelling/provenance-core';
 import { ProvenanceTreeVisualization } from './ProvenanceTreeVisualization';
 
 class Index {
-    private graph = new ProvenanceGraph({ name: 'calculator', version: '1.0.0' });
-    private registry = new ActionFunctionRegistry();
+  private graph = new ProvenanceGraph({ name: 'calculator', version: '1.0.0' });
+  private registry = new ActionFunctionRegistry();
 
-    private tracker = new ProvenanceTracker(this.registry, this.graph);
-    private traverser = new ProvenanceGraphTraverser(this.registry, this.graph);
+  private tracker = new ProvenanceTracker(this.registry, this.graph);
+  private traverser = new ProvenanceGraphTraverser(this.registry, this.graph);
 
-    private app = new Calculator(this.graph, this.registry, this.tracker, this.traverser);
-    private statusDisplay = <HTMLElement>(document.createElement('div'));
-    
-    constructor() {        
-        this.app.setupBasicGraph().then(() => {
-            const provenanceTreeVisualization = new ProvenanceTreeVisualization(this.graph, this.traverser, this.graph.root);
-        });
+  private app = new Calculator(
+    this.graph,
+    this.registry,
+    this.tracker,
+    this.traverser,
+  );
+  private statusDisplay = document.createElement('div') as HTMLElement;
 
-        document.body.appendChild(this.statusDisplay);
-        
-        this.graph.on('currentChanged', (event) => this.displayCurrentState(event, this.app));
-    }
+  constructor() {
+    this.app.setupBasicGraph().then(() => {
+      const provenanceTreeVisualization = new ProvenanceTreeVisualization(
+        this.graph,
+        this.traverser,
+        this.graph.root,
+      );
+    });
 
+    document.body.appendChild(this.statusDisplay);
 
-    displayCurrentState(event: Event, calculator:Calculator) {        
-        this.statusDisplay.innerHTML = `
+    this.graph.on('currentChanged', (event) =>
+      this.displayCurrentState(event, this.app),
+    );
+  }
+
+  public displayCurrentState(event: Event, calculator: Calculator) {
+    this.statusDisplay.innerHTML = `
             <p>${calculator.currentState()}</p>
         `;
-    }
+  }
 }
 
 const index = new Index();
-
-
-
-
