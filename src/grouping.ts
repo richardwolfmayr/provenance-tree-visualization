87852a2247b5b7447d46ea65f45f664535108fac
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2020-05-05 11:00:23 +0200

    removed grouping (for now) by commenting group function to do nothing

--- src/grouping.ts
+++ src/grouping.ts
@@ -4,18 +4,18 @@ export type NodeGroupTest<T> =
   (a: IGroupedTreeNode<T>, b: IGroupedTreeNode<T>) => boolean;
 
 export const group = <T>(node: IGroupedTreeNode<T>, test: NodeGroupTest<T>) => {
-  let merged = false;
-  do {
-    merged = false;
-    for (const child of node.children) {
-      if (test(node, child)) {
-        node.children.splice(node.children.indexOf(child), 1);
-        node.children.push(...child.children);
-        node.wrappedNodes.push(...child.wrappedNodes);
-        merged = true;
-        break;
-      }
-    }
-  } while (merged);
-  node.children.map((child) => group(child, test));
+  // let merged = false;
+  // do {
+  //   merged = false;
+  //   for (const child of node.children) {
+  //     if (test(node, child)) {
+  //       node.children.splice(node.children.indexOf(child), 1);
+  //       node.children.push(...child.children);
+  //       node.wrappedNodes.push(...child.wrappedNodes);
+  //       merged = true;
+  //       break;
+  //     }
+  //   }
+  // } while (merged);
+  // node.children.map((child) => group(child, test));
 };
